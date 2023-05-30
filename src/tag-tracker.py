7dd74885cd621f9a0696a7a008961d5229c66103
jugegp16 <jugegp16@gmail.com>
2023-05-29 20:04:38 -0400

    add utils

--- src/tag-tracker.py
+++ src/tag-tracker.py
@@ -1,11 +1,11 @@
 import argparse
 import calendar
-import json
 import logging
 import os
 import re
 from collections import defaultdict
 from datetime import datetime
+import utils
 
 
 class bcolors:
@@ -23,36 +23,16 @@ class TagTracker:
         self.output_name = output_name
         self.tags = defaultdict(list)
         self.phases = ["to-do", "in-progress", "finished"]
-        self.settings = None
+        self.settings = utils.get_settings()
 
     def __call__(self):
-        self.__parse_settings()
         logging.info(f"{bcolors.OKCYAN}Analyzing...")
         self.index(self.dir, set())
         logging.info(f"{bcolors.OKCYAN}Generating Report...")
         self.output()
 
-    def __parse_settings(self):
-        """
-        parse settings.json and write object to self.settings
-        """
-
-        path = os.path.join(
-            os.path.split(os.path.dirname(__file__))[0], "settings.json"
-        )
-        if os.path.isfile(path):
-            with open(path) as settings:
-                self.settings = json.load(settings)
-
     def index(self, directory: str, found_files: set):
-        """
-        find and index tagged files in input directory
-
-        Args:
-            directory (str): path to index
-            found_files (set): searched file paths
-        """
-
+        """find and index tagged files in input directory"""
         if not os.path.isdir(directory) or ".git" in directory:
             return
 
@@ -63,53 +43,42 @@ class TagTracker:
         # search all files in directory
         for root, dirs, files in os.walk(directory):
             for filename in files:
-                # open file and read contents
                 file_path = os.path.join(root, filename)
-                if file_path not in found_files:
-                    found_files.add(file_path)
+                if file_path in found_files or filename[-3:] != ".md":
+                    continue
 
-                    # only look at .md files
-                    if ".md" not in filename:
-                        continue
+                logging.info(
+                    f"{bcolors.GREY}\t\tIndexing {bcolors.UNDERLINE}{filename}{bcolors.ENDC}"
+                )
 
-                    logging.info(
-                        f"{bcolors.GREY}\t\tIndexing {bcolors.UNDERLINE}{filename}{bcolors.ENDC}"
+                found_files.add(file_path)
+                with open(file_path, "r") as f:
+                    contents = f.read()
+
+                tags = re.findall(r"#\S+", contents)
+                cleaned_tags = [x[1:] for x in tags if "##" not in x]
+
+                for tag in cleaned_tags:
+                    rel_path: str = os.path.relpath(file_path, self.dir)
+                    formatted = "[{}]({})".format(
+                        file_path.split("/")[-1][:-3], rel_path.replace(" ", "%20")
                     )
+                    self.tags[tag].append(formatted)
 
-                    with open(file_path, "r") as f:
-                        # search for tags using regex
-                        contents = f.read()
-                        tags = re.findall(r"#\S+", contents)
-
-                        # index tags -> filename
-                        for tag in tags:
-                            tag = tag[1:]
-
-                            if "#" not in tag:
-                                rel_path: str = os.path.relpath(file_path, self.dir)
-                                formatted = f"[{file_path.split('/')[-1][:-3]}]({rel_path.replace(' ', '%20')})"
-                                self.tags[tag].append(formatted)
-
-            # recursively search subdirectories
+            # search subdirectories recursively
             for subdir in dirs:
                 self.index(os.path.join(directory, subdir), found_files)
 
     def calendar(self, cal_dir="-"):
-        """
-        create calendar view for current month
-
-        Args:
-            cal_dir (str, optional): directory to store daily summaries. Defaults to '-'.
-
-        Returns:
-            calendar (str): calendar string for report
-        """
-
+        """create calendar view for current month"""
         res = ""
         cal = calendar.Calendar()
         today = datetime.today()
-        emdbed_str = (
-            "!" if self.settings and self.settings["embedPagesInDailyView"] else ""
+        emdbed_str = "!" if self.settings["embedPagesInDailyView"] else ""
+        cal_dir = (
+            self.settings["calendarDirectory"]
+            if self.settings["calendarDirectory"]
+            else cal_dir
         )
 
         def output_summary(key):
@@ -117,13 +86,12 @@ class TagTracker:
             if not os.path.isdir(dirpath):
                 os.makedirs(dirpath)
 
+            content = "*{0}*\n\n{1}{2}".format(
+                key, emdbed_str, f"\n{emdbed_str}".join(self.tags[key])
+            )
             filepath = os.path.join(dirpath, f"{key}.md")
             with open(filepath, "w") as output_file:
-                output_file.write(
-                    f"*{key}*\n\n{emdbed_str}"
-                    + f"\n{emdbed_str}".join(self.tags[key])
-                    + "\n"
-                )
+                output_file.write(content)
 
         # get list of tuples representing weeks in current month
         weeks = cal.monthdatescalendar(today.year, today.month)
@@ -162,120 +130,76 @@ class TagTracker:
         return res
 
     def last_opened(self):
-        """
-        create last opened files table for obsidian users
-
-        Returns:
-            res (str): last opened files table
-        """
-
-        res = ""
-        workspace_subpath = ".obsidian/workspace.json"
-        workspace_path = None
-
-        def find_workspace(path: os.path, dirs):
-            res = None
-            for d in map(lambda x: os.path.join(path, x), dirs):
-                if ".git" in d:
-                    continue
-
-                for f in os.listdir(d):
-                    tmp_path = os.path.join(d, f)
-                    if os.path.isfile(tmp_path):
-                        if workspace_subpath in tmp_path:
-                            res = tmp_path
-                            break
-
-                    else:
-                        res = res or find_workspace(d, [f])
-
-            return res
-
-        workspace_path = find_workspace(
-            self.dir,
-            [
-                f
-                for f in os.listdir(self.dir)
-                if os.path.isdir(os.path.join(self.dir, f))
-            ],
-        )
-
-        if not workspace_path:
+        """create last opened files table for obsidian users"""
+        workspace_json = utils.get_workspace(self.dir)
+        if not workspace_json:
             logging.warning(
                 f"{bcolors.WARNING}\tError: No Obsidian workspace.json Detected"
             )
-            return res
+            return ""
 
-        with open(workspace_path) as workspace:
-            json_str = json.load(workspace)
+        files = map(
+            lambda i: f"[{i.split('/')[-1]}]({i.replace(' ', '%20')})",
+            filter(
+                lambda filename: os.path.isfile(os.path.join(self.dir, filename)),
+                workspace_json["lastOpenFiles"],
+            ),
+        )
 
-            files = map(
-                lambda i: f"[{i.split('/')[-1]}]({i.replace(' ', '%20')})",
-                filter(
-                    lambda filename: os.path.isfile(os.path.join(self.dir, filename)),
-                    json_str["lastOpenFiles"],
-                ),
-            )
+        return "\n\n\n----\n\n**Last Opened**\n- " + "\n- ".join(files)
 
-            res += "\n\n\n----\n\n**Last Opened**\n- "
-            res += "\n- ".join(files)
+    def kan_ban(self):
+        """create kan ban board"""
+        max_files = (
+            self.settings["maxFilesShown"] if "maxFilesShown" in self.settings else 10
+        )
+        res = ""
+        for tag, file_paths in sorted(
+            filter(lambda i: i[0] in self.phases, self.tags.items()),
+            key=lambda i: self.phases.index(i[0]),
+        ):
+            if not file_paths:
+                continue
+
+            title = " ".join(map(lambda i: i.capitalize(), tag.split("-")))
+            res += f"\n\n### {title}\n- " + "\n- ".join(file_paths[:max_files])
 
         return res
 
+    def tag_list(self):
+        cleaned_tags = map(
+            lambda i: f"{i[0]} ({len(i[1])})",
+            sorted(
+                filter(
+                    lambda i: not bool(re.search(r"\d{4}-\d{2}-\d{2}", i[0])),
+                    self.tags.items(),
+                ),
+                key=lambda i: len(i[1]),
+                reverse=True,
+            ),
+        )
+        return f"\n\n\n----\n*{', '.join(cleaned_tags)}*\n\n"
+
     def output(self):
-        """
-        write report to output file
-        """
+        """write report to output file"""
         with open(os.path.join(self.dir, self.output_name), "w") as output_file:
-            # section line
             output_file.write("----\n")
 
-            # calendar
             logging.info(f"{bcolors.GREY}\tCreating Calendar")
             output_file.writelines(self.calendar())
 
-            # phase list
             logging.info(f"{bcolors.GREY}\tCreating Kanban")
-            for tag, file_paths in sorted(
-                filter(lambda i: i[0] in self.phases, self.tags.items()),
-                key=lambda i: self.phases.index(i[0]),
-            ):
-                if not file_paths:
-                    continue
+            output_file.write(self.kan_ban())
 
-                title = " ".join(map(lambda i: i.capitalize(), tag.split("-")))
-                output_file.write(f"\n\n### {title}\n- " + "\n- ".join(file_paths))
-
-            # last opened files
             logging.info(f"{bcolors.GREY}\tCreating Recently Opened")
             output_file.write(self.last_opened())
 
-            # tags
             logging.info(f"{bcolors.GREY}\tCreating Tag List")
-            output_file.write(
-                "\n\n\n----\n*"
-                + ", ".join(
-                    map(
-                        lambda i: f"{i[0]} ({len(i[1])})",
-                        sorted(
-                            filter(
-                                lambda i: not bool(
-                                    re.search(r"\d{4}-\d{2}-\d{2}", i[0])
-                                ),
-                                self.tags.items(),
-                            ),
-                            key=lambda i: len(i[1]),
-                            reverse=True,
-                        ),
-                    )
-                )
-                + "*\n\n"
-            )
+            output_file.write(self.tag_list())
 
         logging.info(
-            f"{bcolors.OKGREEN}Success!\n\t",
-            f"{bcolors.GREY}Wrote summary to\n",
-            os.path.join(self.dir, self.output_name),
+            f"{bcolors.OKGREEN}Success!\n\t{bcolors.GREY}Wrote summary to\n"
+            + os.path.join(self.dir, self.output_name),
         )
 
         print(f"Updated {self.output_name}")
